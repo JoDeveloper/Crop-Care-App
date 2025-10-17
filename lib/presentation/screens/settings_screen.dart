30c7a6468b6ab9e0b99eb9330cd396c4727120c1
Yousif Khalid <yousifkhalid544@gmail.com>
2025-10-16 17:25:25 -0700

    finishing settings screen ui

--- lib/presentation/screens/settings_screen.dart
+++ lib/presentation/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:crop_care/core/theme/app_colors.dart';
 import 'package:crop_care/presentation/screens/history_screen.dart';
 import 'package:crop_care/presentation/widgets/settings_section.dart';
 import 'package:crop_care/presentation/widgets/gradient_scaffold.dart';
@@ -80,7 +81,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 ///
                 /// this section is about Help & Support it contains User guid, Contact Support and Rate The App
                 SettingsSections(
-                   icon: Icons.live_help_outlined,
+                  icon: Icons.live_help_outlined,
                   title: 'Help & Support',
                   tiles: [
                     ListTile(
@@ -88,18 +89,21 @@ class _SettingsScreenState extends State<SettingsScreen> {
                       title: Text('User Guid'),
                       subtitle: Text('Learn how to use crop care'),
                       onTap: () {},
+                      trailing: Icon(Icons.arrow_forward_ios),
                     ),
                     ListTile(
                       leading: Icon(Icons.email),
                       title: Text('Contact Support'),
                       subtitle: Text('Get help from our team'),
                       onTap: () {},
+                      trailing: Icon(Icons.arrow_forward_ios),
                     ),
                     ListTile(
                       leading: Icon(Icons.star_rounded),
                       title: Text('Rate The App'),
                       subtitle: Text('Share your feedback'),
                       onTap: () {},
+                      trailing: Icon(Icons.arrow_forward_ios),
                     ),
                   ],
                 ),
@@ -109,7 +113,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 /// this section is about Data & Privacy it contains User guid, Contact Support and Rate The App
                 const SizedBox(height: 20),
                 SettingsSections(
-                   icon: Icons.privacy_tip_outlined,
+                  icon: Icons.privacy_tip_outlined,
                   title: 'data & Privacy',
                   tiles: [
                     ListTile(
@@ -142,26 +146,118 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 ///
                 /// this section is about About Crop Care it contains Summary info about the app and Development Team
                 const SizedBox(height: 20),
-                SettingsSections(
-                   icon: Icons.shield_outlined,
-                  // TODO modify this section as in the figma first app design not the seconf one
-                  title: 'About Crop Care',
-                  tiles: [
-                    ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (ctx) => HistoryScreen()),
-                        );
-                      },
-                      leading: Icon(Icons.groups_outlined),
-                      title: Text('Development Team'),
-                      subtitle: Text('Meet the creators'),
-                      trailing: Icon(Icons.arrow_forward_ios),
+                Row(
+                  children: [
+                    Icon(
+                      color: AppColors.primary,
+                      Icons.shield_outlined,
+                      size: 22,
+                    ),
+                    const SizedBox(width: 8),
+                    Text(
+                      'About Crop Care', // section title
+                      style: TextStyle(
+                        color: const Color.fromARGB(
+                          191,
+                          65,
+                          65,
+                          65,
+                        ), // section title color
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
                     ),
                   ],
                 ),
+                const SizedBox(height: 16),
+                AboutApp(),
               ],
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AboutApp extends StatelessWidget {
+  const AboutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: const Color.fromARGB(193, 237, 245, 235),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: AppColors.primary,
+
+              borderRadius: BorderRadius.circular(200),
+            ),
+            child: Icon(
+              Icons.eco,
+              size: 80,
+              color: const Color.fromARGB(171, 46, 46, 46),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Crop Care',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Verstion 1.0.0',
+            style: TextStyle(
+              fontSize: 16,
+              color: const Color.fromARGB(195, 53, 53, 53),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'AI-Power Plant Disease Detection',
+            style: TextStyle(
+              fontSize: 16,
+              color: const Color.fromARGB(195, 53, 53, 53),
+            ),
+          ),
+          const SizedBox(height: 8),
+          const Divider(
+            thickness: 1,
+            color: Colors.black26,
+            indent: 16,
+            endIndent: 16,
+          ),
+          const SizedBox(height: 8),
+          Text(
+            'Helping farmers identify crop disease with advanced AI technology. Built with care for the agrecultural community.',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 14,
+              color: const Color.fromARGB(195, 53, 53, 53),
+            ),
+          ),
+          const SizedBox(height: 12),
+
+          ListTile(
+            onTap: () {
+              Navigator.of(
+                context,
+              ).push(MaterialPageRoute(builder: (ctx) => HistoryScreen()));
+            },
+            leading: Icon(Icons.groups_outlined),
+            title: Text('Development Team'),
+            subtitle: Text('Meet the creators'),
+            trailing: Icon(Icons.arrow_forward_ios),
           ),
         ],
       ),
